d6fd3243bb5f76829cc10779f81fe97b726509b9
Peter Carbonetto <peter.carbonetto@gmail.com>
2021-01-25 14:16:04 -0600

    Made improvements to compute_cov.R.

--- compute_cov.R
+++ compute_cov.R
@@ -1,23 +1,31 @@
-# Short script to compute the 948 x 948 kinship matrix from the RegMap
-# genotype data.
+# Short script to compute the 948 x 948 covariance matrix from the
+# RegMap genotype data. In genetics, this is sometimes called the
+# "kinship matrix".
 library("data.table")
 
+# So that the results are reproducible, set the seed.
 set.seed(1)
 
-# Here the genotype data are loaded as a matrix of floating-point numbers.
-cat("Reading genotype data.\n")
+# Load the genotype data as an n x m matrix, where n is the number of
+# genetic markers and m is the number of samples.
+cat("Reading genotype data from geno.csv.gz.\n")
 geno <- fread("geno.csv.gz",sep = ",",header = TRUE)
 geno <- as.matrix(geno)
 geno <- t(geno)
 storage.mode(geno) <- "double"
 
+# Select a random subset of 20,000 genetic markers.
 n <- 20000
 i <- sample(nrow(geno),n)
 geno <- geno[i,]
 
-# The kinship matrix is computed by taking the cross-product of the
-# genotype matrix.
-n <- ncol(geno)
-cat(sprintf("Computing %d x %d covariance matrix.\n",n,n))
+# Compute the covariance matrix.
+n <- nrow(geno)
+m <- ncol(geno)
+cat(sprintf("Computing %d x %d covariance matrix using %d markers.\n",m,m,n))
 timing <- system.time(R <- cov(geno))
 print(timing)
+
+# Save the covariance matrix.
+cat("Saving the covariance matrix to cov.Rdata.\n")
+save(list = "R",file = "cov.RData")
