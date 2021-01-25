43b51999a4d63e44c8bd1abf7dba0dd6ae3b95f8
Peter Carbonetto <peter.carbonetto@gmail.com>
2021-01-25 14:44:40 -0600

    Replaced cov with scale followed by tcrossprod in compute_cov.R.

--- compute_cov.R
+++ compute_cov.R
@@ -1,7 +1,7 @@
 # Short script to compute the 948 x 948 covariance matrix from the
 # RegMap genotype data. In genetics, this is sometimes called the
 # "kinship matrix".
-library("data.table")
+library(data.table)
 
 # So that the results are reproducible, set the seed.
 set.seed(1)
@@ -11,20 +11,22 @@ set.seed(1)
 cat("Reading genotype data from geno.csv.gz.\n")
 geno <- fread("geno.csv.gz",sep = ",",header = TRUE)
 geno <- as.matrix(geno)
-geno <- t(geno)
 storage.mode(geno) <- "double"
 
-# Select a random subset of 20,000 genetic markers.
-n <- 20000
-i <- sample(nrow(geno),n)
-geno <- geno[i,]
+# Select a random subset of 200,000 genetic markers.
+n <- 2e5
+i <- sample(ncol(geno),n)
+geno <- geno[,i]
 
 # Compute the covariance matrix.
 n <- nrow(geno)
 m <- ncol(geno)
-cat(sprintf("Computing %d x %d covariance matrix using %d markers.\n",m,m,n))
-timing <- system.time(R <- cov(geno))
-print(timing)
+cat(sprintf("Computing %d x %d covariance matrix using %d markers.\n",n,n,m))
+t0   <- proc.time()
+geno <- scale(geno,center = TRUE,scale = FALSE)
+R    <- tcrossprod(geno)
+t1   <- proc.time()
+print(t1 - t0)
 
 # Save the covariance matrix.
 cat("Saving the covariance matrix to cov.Rdata.\n")
