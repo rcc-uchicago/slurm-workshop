9b6dd42a6c855aef00269388d51ffc82f4890a9b
Peter Carbonetto <peter.carbonetto@gmail.com>
2021-01-25 07:49:29 -0600

    Revised compute_cov.R slightly.

--- compute_cov.R
+++ compute_cov.R
@@ -1,14 +1,9 @@
 # Short script to compute the 948 x 948 kinship matrix from the RegMap
 # genotype data.
-
-# SET UP ENVIRONMENT
-# ------------------
 library("data.table")
 
 set.seed(1)
 
-# IMPORT GENOTYPE DATA
-# --------------------
 # Here the genotype data are loaded as a matrix of floating-point numbers.
 cat("Reading genotype data.\n")
 geno <- fread("geno.csv.gz",sep = ",",header = TRUE)
@@ -16,14 +11,10 @@ geno <- as.matrix(geno)
 geno <- t(geno)
 storage.mode(geno) <- "double"
 
-n <- 40000
-m <- 500
+n <- 20000
 i <- sample(nrow(geno),n)
-j <- sample(ncol(geno),m)
-geno <- geno[i,j]
+geno <- geno[i,]
 
-# COMPUTE KINSHIP MATRIX
-# ----------------------
 # The kinship matrix is computed by taking the cross-product of the
 # genotype matrix.
 n <- ncol(geno)
